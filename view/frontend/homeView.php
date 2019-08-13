8f854a3135eda1c3f000ed5eead3010f65d43014
Julien Barré <julienbarre01@ymail.com>
2019-08-13 16:36:08 +0200

    Maj homeView - Ajout dernieres news

--- view/frontend/homeView.php
+++ view/frontend/homeView.php
@@ -2,7 +2,30 @@
 
 <?php ob_start(); ?>
 <div class="title">
-    <h1>À Kadoc !</h1>
+    <h1>Elle est ou la poulette ?</h1>
+</div>
+
+<div class="home-news-tilte">
+	<h3>Les dernières nouvelles concernant la poulette !</h3>
+    <em>(ou pas)</em>
+</div>
+
+<div class="home-news">
+<?php
+while ($data = $news->fetch())
+{
+?>
+    <div class="h-news">
+        <div class="news-title">
+            <h3><a href="#"><?= $data['title'] ?></a></h3>
+        </div><br>
+        <em>le <?= $data['creation_date_fr'] ?></em>
+
+        <p><?= nl2br($data['small_content']) ?> ... <em><a href="#">Lire la suite</a></em></p>
+    </div><br>
+<?php
+}
+?>
 </div>
 
 <?php $content = ob_get_clean(); ?>
