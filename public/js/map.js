b00b0ef4844d17bf5f641c2f8d92f0bd611169f2
Julien Barré <julienbarre01@ymail.com>
2019-08-29 18:51:05 +0200

    création et affichage de la map en 3 partie : terrain, bordure et falaise, décors.  + création et affichage de la poulette (position random)

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,10 @@
 class CanvasMap {
-    constructor(map, decorations) {
+    constructor(map, border, decorations) {
         this.map = map;
+        this.border = border;
         this.decorations = decorations;
         this.tileset = {};
+        this.poulette = {};
         this.canvas = {};
         this.context = null;
         this.getTileset();
@@ -16,6 +18,39 @@ class CanvasMap {
 
         // largeur du tileset en tiles
         this.tileset.largeur = this.tileset.image.width / 32;
+
+        this.createPoulette();
+    }
+
+    createPoulette() {
+        this.poulette.image = document.createElement('img');
+        this.poulette.image.src = 'game/sprites/poulette.png';
+        this.poulette.image.width = 96;
+        this.poulette.image.height = 128;
+
+        this.poulette.largeur = this.poulette.image.width / 3;
+        this.poulette.hauteur = this.poulette.image.height / 4;
+
+        this.poulette.direction = 2;
+        let getRandomDirection = () => {
+            this.poulette.direction = Math.floor(Math.random() * Math.floor(4));
+        }
+        this.intervalId = setInterval(getRandomDirection, 1000);
+
+
+        this.poulette.position = Math.floor(Math.random() * Math.floor(3));
+        if (this.poulette.position == 0) {
+            this.poulette.x = 13;
+            this.poulette.y = 5;
+        }
+        else if (this.poulette.position == 1) {
+            this.poulette.x = 14;
+            this.poulette.y = 18;
+        }
+        else {
+            this.poulette.x = 22;
+            this.poulette.y = 10;
+        }
 
         this.getCanvas();
     }
@@ -32,10 +67,10 @@ class CanvasMap {
     }
 
     fillMap() {
-        let dessiner = () => {
+        let draw = () => {
             this.drawMap();
         }
-        this.intervalId = setInterval(dessiner, 40);
+        this.intervalId = setInterval(draw, 40);
     }
     
     drawTile(numero, xDestination, yDestination) {        
@@ -52,21 +87,12 @@ class CanvasMap {
         this.context.drawImage(this.tileset.image, xSource, ySource, 32, 32, xDestination, yDestination, 32, 32);        
     }
 
-    drawDecorations(i) {
-        let xSourceEnTiles = this.decorations[i].numero % this.tileset.largeur;
-        if (xSourceEnTiles == 0) {
-            xSourceEnTiles = this.tileset.largeur;
-        }
-
-        let ySourceEnTiles = Math.ceil(this.decorations[i].numero / this.tileset.largeur);
-
-        let xSource = (xSourceEnTiles - 1) * 32;
-        let ySource = (ySourceEnTiles - 1) * 32;
-
-        this.context.drawImage(this.tileset.image, xSource, ySource, 32, 32, (this.decorations[i].x * 32), (this.decorations[i].y * 32), 32, 32);
+    drawPoulette() {
+        this.context.drawImage(this.poulette.image, this.poulette.largeur, this.poulette.hauteur * this.poulette.direction, 32, 32, (this.poulette.x * 32) + 3, (this.poulette.y * 32) + 3, 16, 16);
     }
     
     drawMap() {
+        // terrain
         for (let i = 0; i < this.map.length; i++) {
             let line = this.map[i];
             let y = i * 32;
@@ -74,8 +100,23 @@ class CanvasMap {
                 this.drawTile(line[j], j * 32, y);
             }
         }
+        // bordures et falaises
+        for (let i = 0; i < this.border.length; i++) {
+            let line = this.border[i];
+            let y = i * 32;
+            for (let j = 0; j < line.length; j++) {
+                this.drawTile(line[j], j * 32, y);
+            }
+        }
+        // la poulette qui sera bien cachée par le décors
+        this.drawPoulette();
+        // décors
         for (let i = 0; i < this.decorations.length; i++) {
-            this.drawDecorations(i);
+            let line = this.decorations[i];
+            let y = i * 32;
+            for (let j = 0; j < line.length; j++) {
+                this.drawTile(line[j], j * 32, y);
+            }
         }
     } 
 }
